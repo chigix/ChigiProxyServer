e9024390660a065ddef08810b35e8f02fd7293cf
Richard Lea <chigix@zoho.com>
2015-02-02 13:59:27 +0800

    new: dev: writeBuffer support.
    Signed-off-by: Richard Lea <chigix@zoho.com>

--- src/com/chigix/bio/proxy/channel/Channel.java
+++ src/com/chigix/bio/proxy/channel/Channel.java
@@ -1,9 +1,15 @@
 package com.chigix.bio.proxy.channel;
 
+import com.chigix.bio.proxy.FormatDateTime;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.net.SocketException;
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -13,18 +19,18 @@ import java.util.logging.Logger;
  */
 public class Channel {
 
-    private int closecount;
-
     private final Socket socket;
 
     private InputStream in;
     private OutputStream out;
 
     private boolean isClosedFlag;
+    private final ConcurrentLinkedQueue<Integer> writeBuffer;
 
     public Channel(Socket socket) {
         this.isClosedFlag = false;
         this.socket = socket;
+        this.writeBuffer = new ConcurrentLinkedQueue();
         try {
             this.in = socket.getInputStream();
             this.out = socket.getOutputStream();
@@ -42,10 +48,51 @@ public class Channel {
     }
 
     /**
+     * Push a data in integer into the buffer queue.
+     *
+     * @param data
+     * @throws java.net.SocketException
+     */
+    public void pushToBuffer(int data) throws SocketException {
+        if (this.isClosed()) {
+            throw new SocketException();
+        }
+        this.writeBuffer.add(data);
+    }
+
+    public void pushToBuffer(byte[] data) throws SocketException {
+        List<Integer> copy = new LinkedList<>();
+        for (int i = 0; i < data.length; i++) {
+            copy.add(Byte.valueOf(data[i]).intValue());
+        }
+        if (this.isClosed()) {
+            throw new SocketException();
+        }
+        this.writeBuffer.addAll(copy);
+    }
+
+    /**
+     * Flush the datas in the buffer queue to channel bound.
+     *
+     * @throws IOException
+     */
+    public void flushBuffer() throws IOException {
+        synchronized (this.writeBuffer) {
+            byte[] buffer = new byte[this.writeBuffer.size()];
+            int countdown = 0;
+            Integer buffer_element = null;
+            while ((buffer_element = this.writeBuffer.poll()) != null) {
+                buffer[countdown++] = buffer_element.byteValue();
+            }
+            this.out.write(buffer);
+        }
+    }
+
+    /**
      * Close this channel.
      */
     public void close() {
-        System.out.println("SOCKET CLOSE:" + this.socket.getInetAddress());
+        System.out.println(FormatDateTime.toTimeString(new Date()) + " SOCKET CLOSE:" + this.socket.getInetAddress());
         this.isClosedFlag = true;
         try {
             this.in.close();
@@ -67,9 +114,9 @@ public class Channel {
     }
 
     /**
-     * Returns the raw IP address of this <code>InetAddress</code> object. The
-     * result is in network byte order: the highest order byte of the address is
-     * in <code>getAddress()[0]</code>.
+     * Returns the raw IP address of the <code>InetAddress</code> object for
+     * local address. The result is in network byte order: the highest order
+     * byte of the address is in <code>getAddress()[0]</code>.
      *
      * @return the raw IP address of this object.
      */
@@ -77,6 +124,13 @@ public class Channel {
         return this.socket.getLocalAddress().getAddress();
     }
 
+    /**
+     * Returns the raw IP address of this <code>InetAddress</code> object for
+     * the connected address. The result is in network byte order: the highest
+     * order byte of the address is in <code>getAddress()[0]</code>.
+     *
+     * @return the raw IP address of this object.
+     */
     public byte[] getRemoteAddress() {
         return this.socket.getInetAddress().getAddress();
     }
